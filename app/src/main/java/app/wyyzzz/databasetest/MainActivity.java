797afb489e62e03c2d4d724ba99225708117a64b
吴远洋 <wuyuanyang@wuyuanyangdeMacBook-Pro.local>
2017-10-11 09:59:53 +0800

    delete and query action test

--- app/src/main/java/app/wyyzzz/databasetest/MainActivity.java
+++ app/src/main/java/app/wyyzzz/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package app.wyyzzz.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -56,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
                 ContentValues values = new ContentValues();
                 values.put("price",10.99);
                 db.update("Book",values,"name = ?",new String[]{"The Da Vinci Code"});
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book","pages > ?",new String[] {"500"});
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book 表中的所有数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        //遍历Cursor 对象，取出数据并进行打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("Mainactivity", "book name is " + name);
+                        Log.d("Mainactivity", "book author is " + author);
+                        Log.d("Mainactivity", "book pages is" + pages);
+                        Log.d("Mainactivity", "book price is " + price);
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
